c5cb5e558e8cd97dd12be2e5ae894e5682bcedf5
christianionut0901-create <christianionut0901@gmail.com>
2026-02-28 15:37:48 +0100

    Remove build folder

--- build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_ToDoApp_FILE C:/Users/chris/OneDrive/Documents/ToDoApp/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/ToDoApp.exe)
-set(__QT_DEPLOY_TARGET_ToDoApp_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_ToDoApp_RUNTIME_DLLS C:/Qt/6.10.2/mingw_64/bin/Qt6Core.dll)
